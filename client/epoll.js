4b05acdc7d993d2739be2995d8cee00b1b548b56
choesang <c.tenzin@secure-shopping.at>
2014-02-19 15:35:15 +0100

    initial project

---
+++ client/epoll.js
@@ -0,0 +1,15 @@
+
+  // Template.hello.greeting = function () {
+  //   return "Welcome to epoll.";
+  // };
+  // 
+  // Template.hello.events({
+  //   'click input' : function () {
+  //     // template data, if any, is available in 'this'
+  //     if (typeof console !== 'undefined')
+  //       console.log("I am meteor");
+  //   }
+  // });
+
+
+
---
+++ client/epollclient.js
@@ -0,0 +1,39 @@
+Questions = new Meteor.Collection("questions");
+
+Template.questions.items = function() {
+	return Questions.find({}, {sort: {'submittedOn' : -1 } } );
+};
+
+Template.addquestion.events({
+	'click input.add-question' : function(event) {
+		event.preventDefault();
+		var questionText = document.getElementById("questionText").value;
+		Meteor.call("addQuestion", questionText, function(error, questionId) {
+			console.log('added question with ID ' + questionId);
+		});
+		document.getElementById("questionText").value = "";
+	}
+});
+
+Template.question.events({
+	
+	'click' : function() {
+		Session.set("selected_question", this._id);
+	},
+	
+	'click a.yes' : function(event) {
+		event.preventDefault();
+		if (Meteor.userId()) {
+			var questionId = Session.get("selected_question");
+			console.log('updating yes count for questionId' + questionId);
+			Meteor.call("incrementYesVotes", questionId);
+		}
+	},
+	
+	'click a.no' : function(event) {
+		event.preventDefault();
+		var questionId = Session.get("selected_question");
+		console.log('updating no count for questionId' + questionId);
+		Meteor.call("incrementNoVotes", questionId);
+	}
+});
